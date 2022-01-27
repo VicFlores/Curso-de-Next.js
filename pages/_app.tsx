80b8daaba45084608d92b635b84306e4c6f989ff
Vic Flores <vicsito2014@gmail.com>
2022-01-27 10:01:44 -0600

    curso finished

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import { AppProps } from 'next/app'
 import 'semantic-ui-css/semantic.min.css'
 import '../global.css'
-
 import CartProvider from '@store/Cart'
 
+/* export const reportWebVitals = (metric) => {
+  console.log(metric)
+} */
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  // Aditional props
-  // Aditional layout
+  // Adicional props
+  // Adicional layout
   // Manejar errores - componentDidCatch
   return (
     <CartProvider>
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { GetStaticProps } from 'next'
 import Layout from '@components/Layout/Layout'
 import ProductSummary from '@components/ProductSummary/ProductSummary'
