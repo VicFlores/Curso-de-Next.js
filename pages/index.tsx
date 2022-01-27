9e1e9cb479aefc10dbcd26e10f945bddcc60ae28
Vic Flores <vicsito2014@gmail.com>
2022-01-26 20:53:26 -0600

    render for pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,32 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
 
-const HomePage = () => {
-  const [productList, setProductList] = useState<TProduct[]>([])
+/* export const getServerSideProps = async () => {
+  const response = await fetch('https://curso-de-next-js-pi.vercel.app/api/avo')
+  const { data: productList }: TAPIAvoResponse = await response.json()
 
-  useEffect(() => {
-    window
-      .fetch('/api/avo')
-      .then((response) => response.json())
-      .then(({ data }: TAPIAvoResponse) => {
-        setProductList(data)
-      })
-  }, [])
+  return {
+    props: {
+      productList,
+    },
+  }
+} */
 
+export const getStaticProps = async () => {
+  /* only page */
+  const response = await fetch('https://curso-de-next-js-pi.vercel.app/api/avo')
+  const { data: productList }: TAPIAvoResponse = await response.json()
+
+  return {
+    props: {
+      productList,
+    },
+  }
+}
+
+const HomePage = ({ productList }: { productList: TProduct[] }) => {
   return (
     <Layout>
       <KawaiiHeader />
